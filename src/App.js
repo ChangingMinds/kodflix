5fd765f553fdf15e03f0e5c410b711135f5f85ee
Benedict Ng-Wai <benedict.ngwai@gmail.com>
2018-11-01 12:38:10 +0000

    Added the six title in a 2 rows 3 colume format

--- src/App.js
+++ src/App.js
@@ -9,6 +9,20 @@ class App extends Component {
         <img src={Gladiator} className="App-logo" alt="logo" />
         <p className="App-intro">
         </p>
+        <div>
+      <div class="flex-grid">
+        <div class="col">Black Mirror</div>
+        <div class="col">Breaking Bad</div>
+        <div class="col">Death Note</div>
+      </div>
+      
+      <div class="flex-grid">
+        <div class="col">Game of Thrones</div>
+        <div class="col">Fear the Walking Dead</div>
+        <div class="col">The Wire</div>
+      </div>
+      
+        </div>
       </div>
     );
   }

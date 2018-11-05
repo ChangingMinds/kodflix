24b656a75b1455a74e61f317a9d1b0c6097e6aed
Benedict Ng-Wai <benedict.ngwai@gmail.com>
2018-11-05 23:47:12 +0000

    to get a uniform finish to the site page, I placed new images and kept the original animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import Gladiator from './Gladiator.jpg';
-import bmirror from './bmirror.jpg';
-import breakingbad from './breakingbad.jpg';
-import deathnote from './deathnote.jpg';
-import got from './got.jpg';
-import fotwd from './fotwd.jpg';
-import thewire from './thewire.jpg';
+import bmirror from './blackmirror.jpg';
+import breakingbad from './kingdomofheaven.jpg';
+import deathnote from './graveofthefireflies.jpg';
+import got from './gameofthrones.jpg';
+import fotwd from './walkingdead.jpg';
+import thewire from './thewire2.jpg';
 
 import './App.css';
 
@@ -16,50 +15,22 @@ class App extends Component {
 
         <div>
 
-          <div class="flex-grid-third flex-grid">
-            <div class="col">
-              <img src={bmirror} className="App-logo" alt="logo" />
-              <div class="overlay">
-                <div class="text">Black Mirror</div>
-              </div>
-            </div>
-            <div class="col">
-              <img src={breakingbad} className="App-logo" alt="logo" />
-              <div class="overlay">
-                <div class="text">Breaking Bad</div>
-              </div>
-            </div>
-            <div class="col">
-              <img src={deathnote} className="App-logo" alt="logo" />
-              <div class="overlay">
-                <div class="text">Death Note</div>
-              </div>
-            </div>
+          <div class="flex-grid">
+            <div class="col"><img src={bmirror} className="App-logo" alt="logo" /><div class="overlay"><div class="text">Black Mirror</div></div></div>
+            <div class="col"><img src={breakingbad} className="App-logo" alt="logo" /><div class="overlay"><div class="text">Kingdom of Heaven</div></div></div>
+            <div class="col"><img src={deathnote} className="App-logo" alt="logo" /><div class="overlay"><div class="text">Grave of the Fireflies</div></div></div>
+            <div class="col"><img src={fotwd} className="App-logo" alt="logo" /><div class="overlay"><div class="text">Walking Dead</div></div></div>
+            <div class="col"><img src={thewire} className="App-logo" alt="logo" /><div class="overlay"><div class="text">The Wire</div></div></div>
           </div>
 
-          <div class="flex-grid-third flex-grid">
-            <div class="col">
-              <img src={got} className="App-logo" alt="logo" />
-              <div class="overlay">
-                <div class="text">Game of Thrones</div>
-              </div>
-            </div>
-            <div class="col">
-              <img src={fotwd} className="App-logo" alt="logo" />
-              <div class="overlay">
-                <div class="text">Fear the Walking Dead</div>
-              </div>
-            </div>
-            <div class="col">
-              <img src={thewire} className="App-logo" alt="logo" /><div class="overlay">
-                <div class="text">The Wire</div>
-              </div>
-            </div>
+          <div class="flex-grid">
+            <div class="col"><img src={got} className="App-logo" alt="logo" /></div>
           </div>
+
         </div>
-      </div>
+      </div>   
     );
   }
 }
-
+        
 export default App;

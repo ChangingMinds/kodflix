3d848cf634b2ae0434302194f64b6f6cdc97ec63
Benedict Ng-Wai <benedict.ngwai@gmail.com>
2019-01-28 20:01:04 +0000

    placed in return to homepage

--- src/Details.js
+++ src/Details.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Details = () => {
     return (
         <div><h1>Hello, this will be the details page for each Movie and TV show :)</h1>
         </div>
+        <Link to='/'>Back to home page</Link>
     )
 }
 
